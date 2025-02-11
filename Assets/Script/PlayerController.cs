fde55d021c2b1f5c4bd8e91dd4905de9610bc2eb
Orange-Mango0125 <73291282+Orange-Mango0125@users.noreply.github.com>
2025-02-11 23:45:34 +0900

    7?

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collecions;
+using System.Collecions.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
